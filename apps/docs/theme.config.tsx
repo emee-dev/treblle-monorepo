5b2ec5df0ee184319f90bc6814fa8ba5435773ad
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-10-20 14:31:03 +0100

    updated the docs config

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 //  npx browserslist@latest --update-db
 const config: DocsThemeConfig = {
-	logo: <span>Treblle Express JS</span>,
-	project: {
-		link: "https://github.com/emee_dev/nextra-docs-template",
-	},
-	chat: {
-		link: "https://discord.com",
-	},
-	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
-	footer: {
-		text: (
-			<span>
-				MIT {new Date().getFullYear()} ©{" "}
-				<a href="/" target="_blank">
-					API SIMPLIFIED 🚀
-				</a>
-				.
-			</span>
-		),
-	},
+  logo: <span>Treblle Express 🚀</span>,
+  project: {
+    link: 'https://github.com/emee-dev/treblle-monorepo/tree/main/apps/sdk',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/emee-dev/treblle-monorepo',
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="/" target="_blank">
+          Treblle Express 🚀
+        </a>
+        .
+      </span>
+    ),
+  },
 };
 
 export default config;
