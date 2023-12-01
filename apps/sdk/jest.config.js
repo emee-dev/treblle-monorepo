c0a155ad1388f83294fc68ec578227a424d38e90
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-12-01 12:56:56 +0100

    add tests for treblle utility functions

--- apps/sdk/jest.config.js
+++ apps/sdk/jest.config.js
@@ -12,7 +12,4 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
 };
